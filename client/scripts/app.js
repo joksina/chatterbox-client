cd8793e0bb0fcb88e67d4df0af9d670cf682cc9c
joksina <jksalaudeen@yahoo.com>
2015-12-07 16:48:34 -0800

    Can create messages on the page and they are displayed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,4 @@
-// // YOUR CODE HERE:
+// // YOUR CODE HERE:ne
 var room = "lobby";
 var app = {
   server: 'https://api.parse.com/1/classes/chatterbox'
@@ -7,7 +7,6 @@ app.init = function () {
 }
 app.send = function (message){
   $.ajax({
-//   // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'POST',
     data: JSON.stringify(message),
@@ -16,75 +15,43 @@ app.send = function (message){
       console.log('chatterbox: Message sent');
     },
     error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message');
     }
   });
 }
 app.fetch = function (message){
-    // This is the url you should use to communicate with the parse API server.
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'GET',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
 
-        console.log('chatterbox: Message Recieved');
-      },
-      error: function (data) {
-    // Sees: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.log('chatterbox: Message Recieved');
+    },
+    error: function (data) {
     console.error('chatterbox: Failed to send message');
-  }
-});
+    }
+  });
+};
 
-  }
+app.clearMessages = function (){
+  $('#chats').html("");
+  $('#chats').children = [];
+};
 
-  app.clearMessages = function (){
-    $('#chats').html("");
-    $('#chats').children = [];
-  }
-  // children.prototype.push = function(array){
-  //   array[array.length] = th
-  // }
-  app.addMessage = function (message){
-    $('#chats').append('<div>'+message+'</div>');
-  }
-  app.addRoom = function (roomName){
-    $('#roomSelect').append('<option>'+ roomName + '</option>');
-  }
-// var message = {
-//   username: 'anonlife',
-//   text: 'omg',
-//   roomname: 'myroom'
-// };
-// $.ajax({
-//   // This is the url you should use to communicate with the parse API server.
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent', data);
-//   },
-//   error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });
-// $.ajax({
-//   // This is the url you should use to communicate with the parse API server.
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'GET',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
+app.addMessage = function (message){
+  $('#chats').append('<div> <span class = "userName">'+message.username+': </span>'+'<span class = "message">'+message.text+'</span> </div>');
+};
 
-//     console.log('chatterbox: Message Recieved', data);
-//   },
-//   error: function (data) {
-//     // Sees: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });
+app.addRoom = function (roomName){
+  $('#roomSelect').append('<option>'+ roomName + '</option>');
+};
+$(document).ready(function() {
 
+  $('#submit').click(function (){
+    app.addMessage({username: "hi", text: $('#newMessage').val()})
+      // console.log(())
+  });
+})
+// app.
